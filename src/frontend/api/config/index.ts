4933f2d700b1f126539a0c675d09f0e2d0958f66
James Hoff <jameshoff@microsoft.com>
2022-04-21 08:20:21 -0400

    update

--- src/frontend/api/config/index.ts
+++ src/frontend/api/config/index.ts
@@ -7,16 +7,9 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
             context.log('HTTP trigger function processed a request.');
             //const client = new CosmosClient(process.env.COSMOS_DB_CONNECTION_STRING);
             //context.log(`body : ${JSON.stringify(req.body)}`)
-
-<<<<<<< HEAD
             //const database = client.database(process.env.COSMOS_DB_DB);
             //const container = database.container(process.env.COSMOS_DB_CONTAINER);
             //const item = await container.item("1")
-=======
-            const database = client.database(process.env.COSMOS_DB_DB);
-            const container = database.container(process.env.COSMOS_DB_CONTAINER);
-            const item = await container.item("1")
->>>>>>> 0087382 (update)
             //await item.delete()
             const out = await create(context, req)
             context.res = {
