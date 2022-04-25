103aad53800ce53103cb716b4731ec22c59b988e
James Hoff <jameshoff@microsoft.com>
2022-04-25 15:09:30 -0400

    update

--- src/backend/services/blob.ts
+++ src/backend/services/blob.ts
@@ -1,5 +1,6 @@
-import { BpaServiceObject } from "../engine/types"
+//import { BpaServiceObject } from "../engine/types"
 import { BlobServiceClient, ContainerClient, BlockBlobClient, BlockBlobUploadResponse } from "@azure/storage-blob"
+const PDFDocument = require('pdf-lib').PDFDocument;
 
 export class Blob {
 
@@ -11,19 +12,34 @@ export class Blob {
         this._blobContainerClient = this._blobServiceClient.getContainerClient(containerName);
     }
 
-    public split = async (input : BpaServiceObject) : Promise<BpaServiceObject> => {
-        
-        const blobClient : BlockBlobClient = this._blobContainerClient.getBlockBlobClient(input.projectName)
-        const uploadBlobResponse : BlockBlobUploadResponse = await blobClient.upload(input.data, input.data.length)
-
-        const result : BpaServiceObject = {
-            label : input.label,
-            data : uploadBlobResponse.errorCode,
-            type : "blobUploadError",
-            bpaId : input.bpaId,
-            projectName : input.projectName
+    public split = async (myBlob : Buffer, filename : string) : Promise<void> => {
+        const pages : Buffer[] = await this._splitPdf(myBlob)
+        let index = 0
+        for(const page of pages){
+            const blobClient : BlockBlobClient = this._blobContainerClient.getBlockBlobClient(`${filename.replace(".pdf",`_${index}`)}.pdf`)
+            const uploadBlobResponse : BlockBlobUploadResponse = await blobClient.upload(page, page.length)
+            console.log(`uploadResponse : ${JSON.stringify(uploadBlobResponse)}`)
+            index++
         }
+        
+    }
 
+    private _splitPdf = async (myBlob : Buffer) : Promise<Buffer[]> => {
+
+        const pdfDoc = await PDFDocument.load(myBlob)
+        const numberOfPages = pdfDoc.getPages().length;
+        const result : Buffer[] = []
+        for (let i = 0; i < numberOfPages; i++) {
+            // Create a new "sub" document
+            const subDocument = await PDFDocument.create();
+            // copy the page at current index
+            const [copiedPage] = await subDocument.copyPages(pdfDoc, [i])
+            subDocument.addPage(copiedPage);
+            const pdfBytes = await subDocument.save()
+            result.push(pdfBytes)
+            console.log(`file-${i + 1}.pdf`)
+            //await writePdfBytesToFile(`out/file-${i + 1}.pdf`, pdfBytes);
+        }
         return result
     }
 
