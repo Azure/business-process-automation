43bda1172275fdf3456f23b0e53df381395ab2cb
James Hoff <jameshoff@microsoft.com>
2022-05-03 15:28:27 -0400

    update

--- src/backend/huggingface/preprocess/__init__.py
+++ src/backend/huggingface/preprocess/__init__.py
@@ -6,16 +6,17 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     print('Python HTTP trigger function processed a request.')
     out = {}
     try:
-        req_body = req.get_json()
-        text = req_body.get('text')
-        preprocess = Preprocess()
-        words, filtered_words, stemmed, pos = preprocess.preprocess(text)
-        out = {
-            words : words,
-            filtered_words : filtered_words,
-            stemmed : stemmed,
-            pos : pos
-        }
+        # req_body = req.get_json()
+        # text = req_body.get('text')
+        # preprocess = Preprocess()
+        # words, filtered_words, stemmed, pos = preprocess.preprocess(text)
+        # out = {
+        #     words : words,
+        #     filtered_words : filtered_words,
+        #     stemmed : stemmed,
+        #     pos : pos
+        # }
+        return('hello')
    
     except ValueError:
         print(ValueError)
