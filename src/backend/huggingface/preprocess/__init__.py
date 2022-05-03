8417bdd53cd3499b0a919fdabae8b5947988f156
James Hoff <jameshoff@microsoft.com>
2022-05-03 15:19:08 -0400

    update

--- src/backend/huggingface/preprocess/__init__.py
+++ src/backend/huggingface/preprocess/__init__.py
@@ -1,29 +1,26 @@
 import azure.functions as func
-#from preprocess import Preprocess
+from Preprocess import Preprocess
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    # logging.info('Python HTTP trigger function processed a request.')
-    # print('Python HTTP trigger function processed a request.')
-    # out = {}
-    # try:
-    #     req_body = req.get_json()
-    #     text = req_body.get('text')
-    #     preprocess = Preprocess()
-    #     words, filtered_words, stemmed, pos = preprocess.preprocess(text)
-    #     out = {
-    #         words : words,
-    #         filtered_words : filtered_words,
-    #         stemmed : stemmed,
-    #         pos : pos
-    #     }
+    print('Python HTTP trigger function processed a request.')
+    out = {}
+    try:
+        req_body = req.get_json()
+        text = req_body.get('text')
+        preprocess = Preprocess()
+        words, filtered_words, stemmed, pos = preprocess.preprocess(text)
+        out = {
+            words : words,
+            filtered_words : filtered_words,
+            stemmed : stemmed,
+            pos : pos
+        }
    
-    # except ValueError:
-    #     logging.info(ValueError)
-    #     print(ValueError)
+    except ValueError:
+        print(ValueError)
 
-    # return f'{out}'
-    return 'hello'
+    return f'{out}'
     
 
     
