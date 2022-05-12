ed9e50fcb8dc0e5bb0ca02a50f3967ebbe21e0a3
James Hoff <jameshoff@microsoft.com>
2022-05-12 13:44:10 -0400

    update

--- src/frontend/api/ner/index.ts
+++ src/frontend/api/ner/index.ts
@@ -4,11 +4,10 @@ import { CosmosClient } from "@azure/cosmos";
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     const client = new CosmosClient(process.env.COSMOS_DB_CONNECTION_STRING);
-
     const database = client.database(process.env.COSMOS_DB_DB);
     const container = database.container(process.env.COSMOS_DB_CONTAINER);
     const querySpec = {
-        query: "SELECT * FROM c",
+        query: "SELECT * FROM c WHERE c.label = 'recognizeEntities'",
         parameters: [
 
         ]
