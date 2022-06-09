0d7d181fc207dec5114865922749199c8ee36408
James Hoff <jameshoff@microsoft.com>
2022-06-09 15:32:25 -0400

    update

--- src/frontend/src/ChangeDataDialog.jsx
+++ src/frontend/src/ChangeDataDialog.jsx
@@ -10,7 +10,7 @@ export default function ChangeDataDialog(props) {
     const onDialogSave = (event) => {
         console.log(event)
         const newOption = props.currentOption
-        newOption.serviceSpecificConfig = { to: props.items[selectedDataIndex] }
+        newOption.serviceSpecificConfig = { dataIndex: setSelectedDataIndex }
         newOption.outputTypes = props.items[selectedDataIndex].outputTypes
         props.setHideDialog(true)
         props.addItemToPipeline(newOption)
