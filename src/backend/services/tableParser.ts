22119dee100d3e5444f205ddb03eecafbe811e86
James Hoff <jameshoff@microsoft.com>
2022-12-08 10:12:44 -0500

    add outertext to cells

--- src/backend/services/tableParser.ts
+++ src/backend/services/tableParser.ts
@@ -4,9 +4,9 @@ import { CosmosDB } from "../services/cosmosdb"
 
 export class TableParser {
 
-    private _db : CosmosDB
+    private _db: CosmosDB
 
-    constructor(cosmosDb : CosmosDB) {
+    constructor(cosmosDb: CosmosDB) {
         this._db = cosmosDb
     }
 
@@ -50,18 +50,50 @@ export class TableParser {
             }
 
             tables.push({ company: company, type: type, date: date, table: table, pageContent: pageText })
-            await this._db.create({type: "table", pipeline : input.pipeline, filename : input.filename, data : { company: company, type: type, date: date, table: table, pageContent: pageText } })
+            await this._db.create({ type: "table", pipeline: input.pipeline, filename: input.filename, data: { company: company, type: type, date: date, table: table, pageContent: pageText } })
         }
+
+        // const cells = []
+        // for (const t of tables) {
+        //     for (const c of t.table.cells) {
+        //         cells.push({ company: company, type: type, date: date, cell: c, table: t })
+        //         await this._db.create({ type: "cell", pipeline: input.pipeline, filename: input.filename, data: { company: company, type: type, date: date, cell: c, table: t } })
+        //     }
+        // }
 
         const cells = []
         for (const t of tables) {
+            const cellOuterText: any = []
             for (const c of t.table.cells) {
-                cells.push({ company: company, type: type, date: date, cell: c, table: t })
-                await this._db.create({type: "cell", pipeline : input.pipeline, filename : input.filename, data : { company: company, type: type, date: date, cell: c, table: t } })
+                cellOuterText.push({ row: c.rowIndex, column: c.columnIndex, content: c.content })
+            }
+            for (const c of t.table.cells) {
+                if (c.columnIndex !== 0 || c.rowIndex !== 0) {
+                    const contentArray = cellOuterText.filter((v: any) => {
+                        if (c.columnIndex === v.column && v.row === 0) {
+                            return v
+                        }
+                        if (c.rowIndex === v.row && v.column === 0) {
+                            return v
+                        }
+                    })
+
+                    let text = ""
+                    for (const ca of contentArray) {
+                        text += " " + ca.content
+                    }
+
+                    cells.push({ company: company, type: type, date: date, cell: c, table: t, outerText: text })
+                    await this._db.create({ type: "cell", pipeline: input.pipeline, filename: input.filename, data: { company: company, type: type, date: date, outerText: text, cell: c, table: t } })
+                } else {
+                    cells.push({ company: company, type: type, date: date, cell: c, table: t, outerText: "" })
+                    await this._db.create({ type: "cell", pipeline: input.pipeline, filename: input.filename, data: { company: company, type: type, date: date, outerText: "", cell: c, table: t } })
+                }
             }
         }
 
-        let output : BpaServiceObject = input
+
+        let output: BpaServiceObject = input
         output.index++
         output.label = "tableParser"
 
