6458215d1264e9242ec8d374d7a36a8d0981a415
James Hoff <jameshoff@microsoft.com>
2023-01-30 15:45:20 -0500

    add prompt to openai dialog

--- src/frontend/src/Components/Dialogs/OpenAiGenericDialog.jsx
+++ src/frontend/src/Components/Dialogs/OpenAiGenericDialog.jsx
@@ -1,16 +1,23 @@
-import React, {useState} from 'react'
-import { Dialog, Input, TextArea } from '@fluentui/react-northstar';
+import React, { useState } from 'react'
+import { Dialog, TextArea, Text } from '@fluentui/react-northstar';
 
 
 export default function OpenAiGenericDialog(props) {
 
-    const [prompt, setPrompt] = useState(null)
-    const [maxTokens, setMaxTokens] = useState(null)
+    const [prompt, setPrompt] = useState(`{
+        "prompt": "\${document}\ttldr;",
+        "temperature": 0.7,
+        "top_p": 1,
+        "frequency_penalty": 0,
+        "presence_penalty": 0,
+        "best_of": 1,
+        "max_tokens": 214,
+        "stop": null\n}`)
 
     const onDialogSave = (event) => {
         console.log(event)
         const newOption = props.currentOption
-        newOption.serviceSpecificConfig = { prompt: prompt, maxTokens: maxTokens }
+        newOption.serviceSpecificConfig = prompt
         props.setHideDialog(true)
         props.addItemToPipeline(newOption)
     }
@@ -23,18 +30,16 @@ export default function OpenAiGenericDialog(props) {
         setPrompt(newValue.value)
     }
 
-    const onMaxTokensChange = (_, newValue) => {
-        setMaxTokens(newValue.value)
-    }
-
-
+    const instructions = `From the OpenAI Explorer, replace the text that will represent the plain text of your document or media with the string \${document}.  Press "Code View", select JSON, and copy the contents here.`
     return (
         <Dialog
             header="Generic OpenAI Processing"
-            content={<div style={{display:"flex", flexDirection:"column", textAlign:"left"}}>
-                    <TextArea label="Prompt" labelPosition="inline" value={prompt} onChange={onPromptChange} style={{marginBottom: "10px"}}/>
-                    <Input label="Maximum Tokens"  value={maxTokens} onChange={onMaxTokensChange} />
-                        </div>}
+            content={<div style={{ display: "flex", flexDirection: "column", textAlign: "left" }}>
+                <Text>
+                    {instructions}
+                </Text>
+                <TextArea label="JSON" labelPosition="inline" value={prompt} onChange={onPromptChange} style={{ marginBottom: "10px", height: "400px" }} />
+            </div>}
             open={!props.hideDialog}
             cancelButton="Cancel"
             confirmButton="Submit"
