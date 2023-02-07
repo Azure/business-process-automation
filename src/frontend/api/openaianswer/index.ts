c93c04dbe6a59aae7d945172b59c702578bfa3be
James Hoff <jameshoff@microsoft.com>
2023-02-07 17:39:19 -0500

    bug fix

--- src/frontend/api/openaianswer/index.ts
+++ src/frontend/api/openaianswer/index.ts
@@ -19,7 +19,13 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 
         let url = `${process.env.OPENAI_ENDPOINT}openai/deployments/${process.env.OPENAI_DEPLOYMENT_TEXT}/completions?api-version=2022-12-01`
 
-        const truncatedString = req.body.document.aggregatedResults.ocr.content.slice(0, 3500)
+        let truncatedString = ""
+        if(req?.body?.document?.aggregatedResults?.ocr?.content){
+            truncatedString = req.body.document.aggregatedResults.ocr.content.slice(0, 3500)
+        } else if (req?.body?.document?.aggregatedResults?.speechToText) {
+            truncatedString = req.body.document.aggregatedResults.speechToText.slice(0, 3500)
+        }
+        
         const q = req.body.q
         let body = {
             "prompt": truncatedString + "\n\n\n" + q,
