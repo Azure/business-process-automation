7880cc58e03d41ea255d3c71ba41f5b9801743d8
James Hoff <jameshoff@microsoft.com>
2023-04-14 10:49:19 -0400

    update

--- src/backend/huggingface/chat/__init__.py
+++ src/backend/huggingface/chat/__init__.py
@@ -74,11 +74,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     out = {}
     try:
         
-        if DEV == "true":
+        if DEV == "false":
             req_json = req.get_json()
             req_body = req_json.get("body")
-
-        req_body = req.get_json()
+        else : 
+            req_body = req.get_json()
 
         search_client = SearchClient(
         endpoint=f"https://{AZURE_SEARCH_SERVICE}.search.windows.net",
